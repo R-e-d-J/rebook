76237dbace0549ebe77bbf2257b160910d4d9fb0
Trullemans Gregory <gtrullem@gmail.com>
2021-09-03 08:32:32 +0200

    Constant on module level.

--- test_rebook2.py
+++ test_rebook2.py
@@ -1,46 +1,46 @@
 # Test
-from rebook2 import ReBook, MainFrame
+import rebook2
 k2pdfopt_path = './k2pdfopt'
 
 def test_generate_command_with_empty_default_var_map():
-    rebook = ReBook()
-    frame = MainFrame(rebook, k2pdfopt_path)
+    rebook = rebook2.ReBook()
+    frame = rebook2.MainFrame(rebook, k2pdfopt_path)
     frame.k2pdfopt_command_args = {}
     assert frame.generate_command_argument_string() == '-a- -ui- -x'
 
 def test_generate_command_with_default_var_map():
-    rebook = ReBook()
-    frame = MainFrame(rebook, k2pdfopt_path)
+    rebook = rebook2.ReBook()
+    frame = rebook2.MainFrame(rebook, k2pdfopt_path)
     assert frame.generate_command_argument_string() == '-mode def -ehl 1 -evl 1 -rt 0 -ws 0.200 -sm -wrap+ -dev k2 -a- -ui- -x'
 
 def test_generate_command_for_mode():
-    rebook = ReBook()
-    frame = MainFrame(rebook, k2pdfopt_path)
-    for mode_argument in MainFrame.mode_argument_map:
+    rebook = rebook2.ReBook()
+    frame = rebook2.MainFrame(rebook, k2pdfopt_path)
+    for mode_argument in rebook2.MODE_ARGUMENT_MAP:
         frame.k2pdfopt_command_args = {}
-        frame.k2pdfopt_command_args[frame.CONVERSION_MODE_ARG_NAME] = frame.CONVERSION_MODE_ARG_NAME + ' ' + MainFrame.mode_argument_map[mode_argument]
-        assert frame.generate_command_argument_string() == frame.CONVERSION_MODE_ARG_NAME + ' ' + MainFrame.mode_argument_map[mode_argument] + ' -a- -ui- -x'
+        frame.k2pdfopt_command_args[rebook2.CONVERSION_MODE_ARG_NAME] = rebook2.CONVERSION_MODE_ARG_NAME + ' ' + rebook2.MODE_ARGUMENT_MAP[mode_argument]
+        assert frame.generate_command_argument_string() == rebook2.CONVERSION_MODE_ARG_NAME + ' ' + rebook2.MODE_ARGUMENT_MAP[mode_argument] + ' -a- -ui- -x'
 
 def test_generate_commande_for_device():
-    rebook = ReBook()
-    frame = MainFrame(rebook, k2pdfopt_path)
-    for device in MainFrame.device_argument_map:
-        if MainFrame.device_argument_map[device] is not None:
+    rebook = rebook2.ReBook()
+    frame = rebook2.MainFrame(rebook, k2pdfopt_path)
+    for device in rebook2.DEVICE_ARGUMENT_MAP:
+        if rebook2.DEVICE_ARGUMENT_MAP[device] is not None:
             frame.k2pdfopt_command_args = {}
-            frame.k2pdfopt_command_args[frame.DEVICE_ARG_NAME] = frame.DEVICE_ARG_NAME + ' ' + MainFrame.device_argument_map[device]
-            assert frame.generate_command_argument_string() == frame.DEVICE_ARG_NAME + ' ' + MainFrame.device_argument_map[device] + ' -a- -ui- -x'
+            frame.k2pdfopt_command_args[rebook2.DEVICE_ARG_NAME] = rebook2.DEVICE_ARG_NAME + ' ' + rebook2.DEVICE_ARGUMENT_MAP[device]
+            assert frame.generate_command_argument_string() == rebook2.DEVICE_ARG_NAME + ' ' + rebook2.DEVICE_ARGUMENT_MAP[device] + ' -a- -ui- -x'
 
 def test_generate_commande_device_correspondance():
     ''' Check if for each element of MainFrame.device_choice_map there is an correspondence in
         MainFrame.device_argument_map whitch allow to build a correct command.
     '''
-    rebook = ReBook()
-    frame = MainFrame(rebook, k2pdfopt_path)
-    for device in MainFrame.device_choice_map:
-        if MainFrame.device_choice_map[device] != 'Other (specify width & height)':
+    rebook = rebook2.ReBook()
+    frame = rebook2.MainFrame(rebook, k2pdfopt_path)
+    for device in rebook2.DEVICE_CHOICE_MAP:
+        if rebook2.DEVICE_CHOICE_MAP[device] != 'Other (specify width & height)':
             frame.k2pdfopt_command_args = {}
-            frame.k2pdfopt_command_args[frame.DEVICE_ARG_NAME] = frame.DEVICE_ARG_NAME + ' ' + MainFrame.device_argument_map[device]
-            assert frame.generate_command_argument_string() == frame.DEVICE_ARG_NAME + ' ' + MainFrame.device_argument_map[device] + ' -a- -ui- -x'
+            frame.k2pdfopt_command_args[rebook2.DEVICE_ARG_NAME] = rebook2.DEVICE_ARG_NAME + ' ' + rebook2.DEVICE_ARGUMENT_MAP[device]
+            assert frame.generate_command_argument_string() == rebook2.DEVICE_ARG_NAME + ' ' + rebook2.DEVICE_ARGUMENT_MAP[device] + ' -a- -ui- -x'
 
 # def test_cleaning_command_line_from_cbox():
 #     '''
